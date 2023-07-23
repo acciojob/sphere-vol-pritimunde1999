3ae5922e8ea21119274d2b3e4b89e9c29bafd23a
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-07-23 15:36:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function volume_sphere() {
     //Write your code here
-  
+   var rad = document.getElementById("radius").value;
+	//rad = Math.abs(rad);
+	var val = (4/3) * Math.PI * Math.pow(rad, 3);
+	val = val.toFixed(4);
+	document.getElementById("volume").value = val;
+	return false; // Prevent form submission to avoid page reload
+
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
